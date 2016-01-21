a11146db61f79662df45dc447e220b5ff389ef9b
descent <descent0@gmail.com>
2016-01-21 09:25:59 +0800

    test pure/virtual function

--- driver.cc
+++ driver.cc
@@ -1,6 +1,8 @@
 #include "services.h"
 #include "cpp_support.h"
 
+#define TEST_VF
+
 class Driver {
 public:
 	int x;
@@ -27,6 +29,46 @@ class Obj
 };
 
 
+
+class BaseClass
+{
+  public:
+    BaseClass(int base=5): base_(base)
+    {
+      service_puts("BaseClass\n");
+    }
+    virtual void vfunc()
+    {
+      service_puts("BaseClass vfunc\n");
+    }
+#ifdef TEST_VF
+    virtual void pf()=0;
+#endif
+  private:
+    int base_;
+};
+
+
+class DeriveClass : public BaseClass
+{
+  public:
+    DeriveClass(int d=10): BaseClass(99), d_(d)
+    {
+      service_puts("DeriveClass\n");
+    }
+    virtual void vfunc()
+    {
+      service_puts("DeriveClass vfunc\n");
+    }
+    virtual void pf()
+    {
+      service_puts("DeriveClass pure vfunc\n");
+    }
+  private:
+    int d_;
+};
+
+
 static Driver* driver;
 Driver g_driver;
 Obj obj;
@@ -37,7 +79,23 @@ extern "C" {
         void _GLOBAL__sub_I_g_driver();
 }
 
-int cpp_init() {
+int cpp_init() 
+{
+#ifdef TEST_VF
+  BaseClass *bp = 0;
+#else
+  BaseClass *bp = new BaseClass();
+#endif
+  DeriveClass *dp = new DeriveClass();
+
+#ifndef TEST_VF
+  service_puts("base vfunc\n");
+  bp->vfunc();
+#endif
+  bp = dp;
+  service_puts("derive vfunc\n");
+  bp->vfunc();
+
   _GLOBAL__sub_I_g_driver();
   driver = new Driver();
 
